30dcbc87739dc8454b6e601769f56ae73fb97eae
Jardinesdelareina <jardinesdelareina@mail.ru>
2022-07-08 00:03:23 +0300

    _refactoring 2.0

--- src/feed/services.py
+++ src/feed/services.py
@@ -2,6 +2,6 @@ from src.wall.models import Post
 
 
 def feed(user):
-    # Сортировка постов подписчиков пользователя по дате создания (лента новостей)
+    # Вывод постов всех подписчиков пользователя с сортировкой по дате создания (лента новостей)
     posts = Post.objects.filter(user__owner__subscriber_id=1).order_by('-create_date')\
         .select_related('user').prefetch_related('comments')
